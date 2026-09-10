47ce2deb964015f673f5a7931e77e96dbbdd6767
Amit Kushwaha <amitkushwaha0102@gmail.com>
2026-09-10 07:29:15 +0000

    Fix OIDC thumbprint auto-fetch

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -207,10 +207,14 @@ resource "aws_iam_role_policy" "ecs_exec" {
   })
 }
 
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 resource "aws_iam_role" "github_actions" {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -14,5 +14,9 @@ terraform {
       source  = "hashicorp/archive"
       version = "~> 2.4"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
